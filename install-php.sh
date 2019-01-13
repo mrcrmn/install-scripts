242edd24d5443b56201dd75f4e9090f41b4e57a5
mrcrmn <marcoreimann@outlook.de>
2019-01-13 19:36:22 +0100

    first commit

---
+++ install-php.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Repositories
+sudo add-apt-repository --yes universe
+sudo add-apt-repository --yes ppa:certbot/certbot
+sudo add-apt-repository --yes ppa:ondrej/php
+
+# Updates
+sudo apt-get update --yes
+sudo apt-get upgrade --yes
+
+# Basic stuff and nginx
+sudo apt-get install --yes git curl zip nginx software-properties-common
+
+# Certbot
+sudo apt-get install --yes python-certbot-nginx 
+
+# PHP
+sudo apt-get install --yes php7.3-fpm php7.3-common php7.3-xml php7.3-mbstring php7.3-zip 
+
+# Composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+sudo chmod 777 -R /var/www
---
+++ new-site.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Functions
+ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
+
+# Variables
+NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
+NGINX_ENABLED_VHOSTS='/etc/nginx/sites-enabled'
+WEB_DIR='/var/www'
+WEB_USER='www-data'
+NGINX_SCHEME='$scheme'
+NGINX_REQUEST_URI='$request_uri'
+
+# Sanity check
+[ $(id -g) != "0" ] && die "Script must be run as root."
+[ $# != "1" ] && die "Usage: $(basename $0) domainName"
+
+# Create nginx config file
+cat > $NGINX_AVAILABLE_VHOSTS/$1 <<EOF
+server {
+    listen 80;
+    server_name $1;
+    root        /var/www/$1/public;
+
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Content-Type-Options "nosniff";
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    location ~ \.php$ {
+        fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi.conf;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
+    }
+}
+EOF
+
+# Changing permissions
+chown -R $WEB_USER $WEB_DIR/$1
+chmod 775 -R $WEB_USER $WEB_DIR/$1
+
+# Symlink
+ln -s $NGINX_AVAILABLE_VHOSTS/$1 $NGINX_ENABLED_VHOSTS
+
+ok "Site Created for $1"
