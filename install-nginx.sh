8291855ba861d8dcc51a4e16f1408c2c50a07617
Marco Reimann <marcoreimann@outlook.de>
2019-01-25 21:11:40 +0100

    added php-gd extension

--- install-nginx.sh
+++ install-nginx.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y python-certbot-nginx
 
 # PHP
 
-sudo apt-get install -y php7.3-fpm php7.3-common php7.3-mysql php7.3-bcmath php7.3-xml php7.3-mbstring php7.3-zip php7.3-redis php7.3-curl
+sudo apt-get install -y php7.3-fpm php7.3-common php7.3-mysql php7.3-bcmath php7.3-xml php7.3-mbstring php7.3-zip php7.3-redis php7.3-curl php7.3-gd
 
 # Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
