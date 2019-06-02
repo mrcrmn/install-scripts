2513e9dd1ee114e3c8adcd6be89fbf73d30ec703
Marco R <marcoreimann@outlook.de>
2019-06-02 11:07:09 +0200

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -2,6 +2,13 @@
 
 sudo apt-get -y update
 
+# Node v12
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash
 
 sudo apt-get install -y nodejs
+
+# Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install -y yarn
